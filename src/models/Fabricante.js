944e81694f359dbc47dd792bf3baf2f8caca9422
andmatsou <andmatsou@gmail.com>
2022-07-26 17:00:45 -0300

    feat: Adicionado fabricante model.

---
+++ src/models/Fabricante.js
@@ -0,0 +1,32 @@
+const {Sequelize, DataTypes} = require('sequelize');
+const db = require('./db');
+const Modelo = require('./Modelo');
+
+const Fabricante = db.define('fabricantes', {
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome_fabricante:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  status: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  }
+});
+
+Fabricante.hasMany(Modelo, {
+  constraints: true,
+  foreignKey: 'fabricanteId'
+});
+
+Modelo.belongsTo(Fabricante);
+
+//Fabricante.sync();
+//Fabricante.sync({ alter: true });
+
+module.exports = Fabricante;
