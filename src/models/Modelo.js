c6d538811a5c43435e2defe53cb51b7385482e74
andmatsou <andmatsou@gmail.com>
2022-07-26 17:00:18 -0300

    feat: Adicionado Modelo model.

---
+++ src/models/Modelo.js
@@ -0,0 +1,27 @@
+const {Sequelize, DataTypes} = require("sequelize");
+const db = require("./db");
+
+const Modelo = db.define('modelos', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome_modelo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  fabricanteId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+//Aula.belongsTo(Modulo);
+//Modulo.hasOne(Curso);
+
+//Modelo.sync();
+//Modelo.sync({ alter: true });
+
+module.exports = Modelo;
